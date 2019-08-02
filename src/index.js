0a5cb7aea027aab34b5202fc57909c1664a611e9
Ignacio <shoik.xtazy@gmail.com>
2019-08-01 23:48:09 -0400

    added files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-
-//settings
-app.set('port', process.env.PORT || 3000);
-
-//routes
-app.use('/api/',require('./routes/index'));
-
-app.listen(app.get('port'), () => {
-    console.log("server on port", app.get('port'));
-});
--- src/server/config.js
+++ src/server/config.js
@@ -12,7 +12,26 @@ module.exports = app => {
     app.set('port', process.env.PORT || 3000 );
     app.set('views', path.join(__dirname,'../views'));
     app.engine('.hbs', exphbs({
-        defaultLayout: 'main'
+        defaultLayout: 'main',
+        partialsDir: path.join(app.get('views'),'partials'),
+        layoutDir: path.join(app.get('views','layouts')),
+        extname: '.hbs',
+        helpers: require('.helpers')
     }))
 
+    app.set('view engine', '.hbs');
+
+    //middlewares
+    app.use(morgan('dev'));
+    app.use(express.urlencoded({extended:false}));
+	app.use(express.json());
+
+    //routes
+    routes(app);
+    
+    //static files
+    app.use('/public',express.static(path.join(__dirname,'../public')));
+    
+    return app;
+    
 }
